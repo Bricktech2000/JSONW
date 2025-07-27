af336d78e7f67a7eb0290d06ecd17cc04f5a6f12
Emilien Breton <bricktech2000@gmail.com>
2025-07-27 03:11:21 -0400

    implemented quoting of character and string literals

--- test.c
+++ test.c
@@ -1,11 +1,58 @@
 #include "jsonw.h"
 #include <stdio.h>
+
+char *json = "[{ \"name\": \"John\", \"birth\": 1978 }, { \"birth\": 2010 }, "
+             "{ \"name\": \"too long\" }, { \"birth\": 1.2 }, { \"age\": 5 }]";
+
+struct person {
+  char name[8];
+  short birth;
+};
+
+char *deserialize_person(struct person *person, char **warn, char *json) {
+  for (char *memb = jsonw_beginobj(json); memb; memb = jsonw_member(memb)) {
+    if (jsonw_strcmp("name", jsonw_beginstr(memb)) == 0) {
+      if (jsonw_endstr(jsonw_unescape(person->name, sizeof(person->name),
+                                      jsonw_beginstr(jsonw_name(memb)))))
+        continue;
+      *warn = "invalid name";
+    }
+
+    if (jsonw_strcmp("birth", jsonw_beginstr(memb)) == 0) {
+      double birth;
+      if (jsonw_number(&birth, jsonw_name(memb)) &&
+          (person->birth = birth) == birth)
+        continue;
+      *warn = "invalid birth";
+    }
+
+    if (*warn == NULL)
+      *warn = "unknown key";
+  }
+
+  return jsonw_object(NULL, json);
+}
+
+int main(void) {
+  for (char *elem = jsonw_beginarr(json); elem; elem = jsonw_element(elem)) {
+    struct person person = {.name = "", .birth = 0};
+    char *warn = NULL;
+    if (deserialize_person(&person, &warn, elem)) {
+      printf("(struct person){\"%s\", %d}", person.name, person.birth);
+      printf(warn ? " (%s)\n" : "\n", warn);
+    }
+  }
+}
+
+/*
+#include "jsonw.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
   long size;
   char *buf = NULL;
-  for (argv++; *argv; argv++) {
+  while (*++argv) {
     // printf("parsing %s\n", *argv);
 
     FILE *fp = fopen(*argv, "r");
@@ -34,3 +81,4 @@ int main(int argc, char **argv) {
 
   free(buf);
 }
+*/
